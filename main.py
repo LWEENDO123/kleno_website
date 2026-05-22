30357b17397da2b4299fe3e11a02c0c804088dd4
LWEENDO123 <57754490+LWEENDO123@users.noreply.github.com>
2026-05-22 05:12:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from botocore.client import Config
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse, RedirectResponse
